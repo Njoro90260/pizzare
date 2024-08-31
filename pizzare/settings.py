99e0041ec14538e5289fe622fb730c12efea1fe7
peter <peter@kali>
2024-08-31 11:57:37 +0300

    style the pages

--- pizzare/settings.py
+++ pizzare/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # My apps
     'pizzas',
+    # Third party apps
+    'bootstrap5',
     # default apps
     'django.contrib.admin',
     'django.contrib.auth',
